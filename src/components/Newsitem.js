7041e3889bab3a953253a5f10a56de52ee7228b9
AbhishekH742 <abhishekhallur742@gmail.com>
2023-12-23 17:04:38 +0530

    news headline card is added

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
   render() {
+    let { tittle, description, imgUrl, newsUrl } = this.props;
     return (
-      <div>
-        This is news item
+      <div className='container my-3'>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={imgUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{tittle}</h5>
+            <p className="card-text">{description}</p>
+            <a href={newsUrl} className="btn btn-sm btn-primary">Read more</a>
+          </div>
+        </div>
       </div>
     )
   }
